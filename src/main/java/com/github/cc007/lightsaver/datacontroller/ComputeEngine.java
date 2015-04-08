b4ba56634e1a33e3c4a8bdfd41baa69fd358cc07
Aerylia <Aerylia@Aerylia-Desktop>
2015-04-08 14:06:42 +0200

    Added classes for RMI implementation. Only copied them from the tutorial, they are not functional yet.

---
+++ src/main/java/com/github/cc007/lightsaver/datacontroller/ComputeEngine.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.cc007.lightsaver.datacontroller;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import org.rug.netcomputing.rmi.base.Compute;
+import org.rug.netcomputing.rmi.base.Task;
+
+/**
+ *
+ * @author Aerylia
+ */
+public class ComputeEngine implements Compute {
+
+    public ComputeEngine() {
+        super();
+    }
+
+    @Override
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
+
+    public static void main(String[] args) {
+        //TODO make this work with everything else (just code from the tutorial
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+            String name = "Compute";
+            Compute engine = new ComputeEngine();
+            Compute stub =
+                (Compute) UnicastRemoteObject.exportObject(engine, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/github/cc007/lightsaver/datacontroller/ComputeEngineStarter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.cc007.lightsaver.datacontroller;
+
+import org.rug.netcomputing.rmi.base.RmiStarter;
+/**
+ *
+ * @author Aerylia
+ */
+public class ComputeEngineStarter extends RmiStarter{
+
+    @Override
+    public void start() {
+        //TODO
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
