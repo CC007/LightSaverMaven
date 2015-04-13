90a8c2df0cd82dd050744560b69c2a1962d12aea
CC007 <Coolcat_the_best2@hotmail.com>
2015-04-13 15:42:03 +0200

    refactor name

--- src/main/java/com/github/cc007/lightsaver/datacontroller/task/CalculateTime.java
+++ src/main/java/com/github/cc007/lightsaver/datacontroller/task/CalculateTime.java
@@ -8,10 +8,8 @@ package com.github.cc007.lightsaver.datacontroller.task;
 import com.github.cc007.lightsaver.appliance.light.Light;
 import com.github.cc007.lightsaver.datacontroller.DataController;
 import com.github.cc007.lightsaver.datacontroller.storage.Entry;
-import com.github.cc007.lightsaver.datacontroller.task.DataTask;
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -30,9 +28,9 @@ public class CalculateTime implements DataTask<Integer>, Serializable {
     private int mode;
     private long startDate;
     private long endDate;
-    private int applianceID;
+    private int applianceId;
 
-    DataController dc;
+    private DataController dc;
 
     public CalculateTime(int mode) {
         this(mode, 0, Calendar.getInstance().getTimeInMillis(), 0);
@@ -42,11 +40,11 @@ public class CalculateTime implements DataTask<Integer>, Serializable {
         this(mode, startDate, endDate, 0);
     }
 
-    public CalculateTime(int mode, long startDate, long endDate, int applianceID) {
+    public CalculateTime(int mode, long startDate, long endDate, int applianceId) {
         this.mode = mode;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.applianceID = applianceID;
+        this.applianceId = applianceId;
     }
 
     @Override
@@ -57,7 +55,7 @@ public class CalculateTime implements DataTask<Integer>, Serializable {
     private Integer computeTime() {
         Integer returnValue = 0;
 
-        Set<List<Entry>> entries = dc.getEntries(startDate, endDate, applianceID);
+        Set<List<Entry>> entries = dc.getEntries(startDate, endDate, applianceId);
         
         Integer accumTime = 0;
         Integer usage = 0;
